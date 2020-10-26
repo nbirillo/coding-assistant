f328b93fa6329f58beeb0c02ee4f407820ac8042
Anastasiia.Birillo <nbirillo@mail.ru>
2020-10-26 12:07:26 +0300

    Fix testsL add Test annotation for the test method

--- src/test/kotlin/org/jetbrains/research/ml/coding/assistant/tree/PsiTreeConverterTest.kt
+++ src/test/kotlin/org/jetbrains/research/ml/coding/assistant/tree/PsiTreeConverterTest.kt
@@ -12,6 +12,7 @@ import org.jetbrains.research.ml.coding.assistant.util.FileTestUtil.content
 import org.jetbrains.research.ml.coding.assistant.util.FileTestUtil.getInAndOutFilesMap
 import org.jetbrains.research.ml.coding.assistant.util.ParametrizedBaseTest
 import org.jetbrains.research.ml.coding.assistant.util.PsiTestUtil.equalTreeStructure
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import java.io.File
@@ -42,6 +43,7 @@ class PsiTreeConverterTest : ParametrizedBaseTest(getResourcesRootPath(::PsiTree
     @Parameterized.Parameter(2)
     var numbering: Numbering? = null
 
+    @Test
     fun `converting PSI to GumTree tree test`() {
         val inFilePsi = myFixture.configureByFile(inSourceFile!!.absolutePath.replace(testDataPath, ""))
         val inContext = ApplicationManager.getApplication().runReadAction<TreeContext> {
