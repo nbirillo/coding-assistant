0a58efc60fd8d4b5157dc31f8b68ffbd39785aa3
Artem Bobrov <artembobrov3059@gmail.com>
2021-03-18 05:08:48 +0300

    report

---
+++ src/main/kotlin/org/jetbrains/research/ml/coding/assistant/report/HintReport.kt
@@ -0,0 +1,17 @@
+package org.jetbrains.research.ml.coding.assistant.report
+
+import org.jetbrains.research.ml.coding.assistant.dataset.model.DatasetTask
+import org.jetbrains.research.ml.coding.assistant.solutionSpace.SolutionSpace
+import org.jetbrains.research.ml.coding.assistant.solutionSpace.SolutionSpaceVertex
+
+/**
+ * Model that represents algorithm's result
+ */
+data class HintReport(
+    val taskName: DatasetTask,
+    val algorithmName: String,
+    val studentCode: String,
+    val space: SolutionSpace,
+    val closestVertex: SolutionSpaceVertex,
+    val nextNode: SolutionSpaceVertex?
+)
---
+++ src/main/kotlin/org/jetbrains/research/ml/coding/assistant/report/HintReportGenerator.kt
@@ -0,0 +1,56 @@
+package org.jetbrains.research.ml.coding.assistant.report
+
+import org.jetbrains.research.ml.coding.assistant.solutionSpace.SolutionSpaceVertex
+import java.io.OutputStream
+
+/**
+ *  Generates algorithm's result report and writes it into outputStream
+ */
+interface HintReportGenerator {
+    fun generate(outputStream: OutputStream, report: HintReport)
+}
+
+class MarkdownHintReportGenerator(private val codeRepository: CodeRepository) : HintReportGenerator {
+    override fun generate(outputStream: OutputStream, report: HintReport) {
+        val text = """
+## ${report.algorithmName}
+
+### Student code
+```python
+${report.studentCode}
+```
+
+### Closest node's code
+`vertex` = ${report.closestVertex}
+
+```python
+${fetchCode(
+            report.closestVertex
+        )}
+```
+
+### Hint node's code 
+`vertex` = ${report.nextNode}
+```python
+${report.nextNode?.let(this::fetchCode) ?: "No Hint Code"}
+```
+""".trimIndent()
+        outputStream.write(text.toByteArray())
+    }
+
+    private fun fetchCode(vertex: SolutionSpaceVertex): String {
+        return codeRepository.getCode(vertex.info.first().id)
+    }
+}
+
+class CompositeMarkdownHintReportGenerator(private val generator: HintReportGenerator) {
+    fun generate(outputStream: OutputStream, reports: Collection<HintReport>) {
+        val firstReport = reports.firstOrNull() ?: return
+        outputStream.write(
+            "# ${firstReport.taskName}\n".toByteArray()
+        )
+        for (report in reports) {
+            generator.generate(outputStream, report)
+        }
+    }
+}
